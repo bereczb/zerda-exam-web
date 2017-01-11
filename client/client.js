de39984288b68bf17ddcb731760eff867d82a336
bereczb <balazs.berecz@gmail.com>
2017-01-11 12:59:28 +0100

    server response is not ready - needed 15 minutes

--- client/client.js
+++ client/client.js
@@ -1,1 +1,41 @@
 'use strict';
+
+var button = document.querySelector('button');
+var experience = document.querySelector('textarea');
+var scale = document.querySelector('#grade');
+var email = document.querySelector('#email');
+var loading = document.querySelector('.hide');
+var list = document.querySelector('ul');
+var rawData;
+var answer = [];
+
+button.addEventListener('click', function () {
+  loading.classList.remove('hide');
+
+  var httpRequest = new XMLHttpRequest();
+  httpRequest.open('POST', 'http://localhost:3000/exam', true);
+  httpRequest.setRequestHeader('Content-Type', 'application/json');
+  httpRequest.send(JSON.stringify({
+    feedback: experience.value,
+    scale: scale.value,
+    email: email.value
+  }));
+
+  httpRequest.onreadystatechange = function () {
+    if (httpRequest.readyState === XMLHttpRequest.DONE) {
+        if (httpRequest.status === 200) {
+         rawData = JSON.parse(httpRequest.responseText);
+         console.log(rawData);
+         if (rawData.message) {
+            answer = [rawData.status, rawData.message];
+         } else {
+            answer = [rawData.status, rawData.projects];
+         }
+          list.innerHTML += '<li>' + answer + '</li>';
+          loading.classList.add('hide');
+        } else {
+          alert('There was a problem with the request.', httpRequest.readyState);
+        }
+      }
+   };
+});
--- server.js
+++ server.js
@@ -2,12 +2,42 @@
 
 var express = require('express');
 var app = express();
+var cors = require('cors');
+var mysql = require('mysql');
+var bodyParser = require('body-parser');
 var validator = require('./validator.js');
+var projects = [];
 
+var connection = mysql.createConnection({
+   host: 'localhost',
+   user: 'root',
+   password: 'admin',
+   database: 'secretprojects'
+});
+
+connection.connect();
+
+var app = express();
 app.use(express.static('client'));
+app.use(bodyParser.json());
 
 app.post('/exam/', function(req, res) {
-   var validator = validator(req.body.feedback, req.body.scale, req.body.email);
+   var validation = validator(req.body.feedback, req.body.scale, req.body.email);
+   var response = { "status": "error", "message": "thank you" }
+   if (validation) {
+      connection.query('SELECT * FROM projects', function(err, rows, fields) {
+         if (err) throw err;
+         console.log(rows);
+         
+
+         projects.push(rows[1].project_name)
+         console.log('projects', projects);
+         response = { "status": "ok", "projects": projects }
+         res.send(response);
+      });
+   } else {
+      res.send(response);
+   }
 });
 
 app.listen(3000, function () {
